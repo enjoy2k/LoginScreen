690fa78f8227b2e8a7c3871314dc9c585d7ae01f
enjoy2k <Enjoy2k@mail.ru>
2022-10-04 10:34:06 +0300

    Сделал 2 экрана

---
+++ LoginScreen/GreetingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GreetingsViewController.swift
+//  LoginScreen
+//
+//  Created by Даниил Козлов on 04.10.2022.
+//
+
+import UIKit
+
+class GreetingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
