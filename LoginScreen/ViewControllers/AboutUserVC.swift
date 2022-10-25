3422e49aaddf9f7ce5bed709550316c32d45a074
enjoy2k <Enjoy2k@mail.ru>
2022-10-25 17:16:23 +0300

    Шуршу

---
+++ LoginScreen/ViewControllers/AboutUserVC.swift
@@ -0,0 +1,17 @@
+//
+//  AboutUserVC.swift
+//  LoginScreen
+//
+//  Created by Даниил Козлов on 25.10.2022.
+//
+
+import UIKit
+
+class AboutUserVC: UIViewController {
+
+    @IBOutlet var aboutUserLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ LoginScreen/ViewControllers/PhotosPageVC.swift
@@ -0,0 +1,18 @@
+//
+//  PhotosPageVC.swift
+//  LoginScreen
+//
+//  Created by Даниил Козлов on 25.10.2022.
+//
+
+import UIKit
+
+class PhotosPageVC: UIViewController {
+
+    @IBOutlet var imagePhotoView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
