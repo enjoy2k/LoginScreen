9fa1b1a462f065658078a134e4fdef8f5ea2842b
enjoy2k <Enjoy2k@mail.ru>
2022-10-30 16:19:29 +0300

    с

---
+++ LoginScreen/Models/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  LoginScreen
+//
+//  Created by Даниил Козлов on 28.10.2022.
+//
+
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+}
